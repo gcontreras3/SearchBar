351223f37c298d9720cb85f8863c5665ab349ad9
Ivan Contreras <gcontreras31996@gmail.com>
2023-06-01 18:00:17 -0500

    adding event listeners to buttons

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ contacts.push({
     phone: 13412341341
 })
 console.log(contacts)
+document.querySelector('#add-contact').addEventListener('click', function(e){
+    e.target.textContent = 'Button was clicked!'
+})
+
+// document.querySelector('#add-contact').addEventListener('click', function(contact){
+//    console.log(`this button was clicked`)
+//     const p = document.createElement('p')
+//         p.textContent = contact.text
+//         document.querySelector('#list')
+//     console.log(contacts)
+// })
