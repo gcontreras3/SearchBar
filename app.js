cc1e593eb878e4840da720a3f0d69c64a5fe7023
Ivan Contreras <gcontreras31996@gmail.com>
2023-06-08 09:13:29 -0500

    finding events

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const contacts = [{}]
 
 
-
-
 // Figuring out how to push an object into an array
+// console.log(contacts)
 
-contacts.push({
-    name: 'Jerry',
-    phone: 13412341341
-})
-console.log(contacts)
 document.querySelector('#add-contact').addEventListener('click', function(e){
     e.target.textContent = 'Button was clicked!'
 })
 
-// document.querySelector('#add-contact').addEventListener('click', function(contact){
-//    console.log(`this button was clicked`)
-//     const p = document.createElement('p')
-//         p.textContent = contact.text
-//         document.querySelector('#list')
-//     console.log(contacts)
-// })
+document.querySelector('#add-contact').addEventListener('click', function(contact){
+   console.log(`this button was clicked`)
+    const li = document.createElement('li')
+        li.textContent = contact
+        document.querySelector('#list')
+    console.log(contacts)
+})
+
+const addContact = document.querySelector('#ordered-list')
+const searchInput = document.querySelector('#search-input').addEventListener('input', function(e){
+    e.target.value
+    console.log(e.target.value)
+})
+
+const phoneNumberInput = document.querySelector('#phone-number').addEventListener('input', function(e){
+    console.log(e.target.value)
+    e.target.value
+})
+
+document.querySelector('#add-contact').addEventListener('click', function(e){
+    addContact.push(searchInput)
+    document.getElementById('#ordered-list').textContent()
+})
