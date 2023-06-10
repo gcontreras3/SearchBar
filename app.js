706056d287fb0ab75a9c1ccc71f3806ca14cda52
Ivan Contreras <gcontreras31996@gmail.com>
2023-06-09 23:56:43 -0500

    testing features that add input to a list

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const contacts = [{}]
+const contacts = [{
+    name: '',
+    phone: ''
+}]
 
 
 // Figuring out how to push an object into an array
+const testItem = document.createElement('li');
+    
+    
+document.querySelector('#test-input').addEventListener('input', function(e){
+    console.log(e.target.value)
+    testItem.textContent = e.target.value
+    document.querySelector('#testOL').appendChild(testItem)
+})
+
+
+
+
+
 // console.log(contacts)
 
 document.querySelector('#add-contact').addEventListener('click', function(e){
@@ -17,11 +33,21 @@ document.querySelector('#add-contact').addEventListener('click', function(contac
 })
 
 const addContact = document.querySelector('#ordered-list')
+
 const searchInput = document.querySelector('#search-input').addEventListener('input', function(e){
     e.target.value
     console.log(e.target.value)
 })
-
+function addContactBtn(){
+    if(seachInput.value === ''){
+        alert("Enter a contact name")
+    }
+    else {
+        let li = document.createElement('li')
+        li.innerHTML = searchInput.value
+        addContact.appendChild(li)
+    }
+}
 const phoneNumberInput = document.querySelector('#phone-number').addEventListener('input', function(e){
     console.log(e.target.value)
     e.target.value
@@ -30,4 +56,32 @@ const phoneNumberInput = document.querySelector('#phone-number').addEventListene
 document.querySelector('#add-contact').addEventListener('click', function(e){
     addContact.push(searchInput)
     document.getElementById('#ordered-list').textContent()
+})
+
+
+const toggleBtn = document.querySelector('#toggleBtn')
+const divList = document.querySelector('.listHolder')
+
+toggleBtn.addEventListener('click', () => {
+    if(divList.style.dipslay === 'none'){
+        divList.style.display = 'block'
+        toggleBtn.innerHTML = 'Hide List'
+    } else {
+        divList.style.display = 'none'
+        toggleBtn.innerHTML = 'Show List'
+    }
+})
+
+// add list items
+
+// create variables
+
+const addInput = document.querySelector('#addInput')
+const addBtn = document.querySelector('#addBtn')
+
+addBtn.addEventListener('click', () => {
+    const ul = divList.querySelector('ul')
+    const li = doucment.createElement('li')
+    li.innerHTML = addInput.value
+    ul.appendChild(li)
 })
